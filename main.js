be89815ec60a613a35aeb272ae57b00f7d55f60f
Blooming Web-Developer <143939385+AnjaliTambade@users.noreply.github.com>
2024-04-12 13:15:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// variables used //
+var settingsMenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+
+// drop-down //
+
+function settingsMenuToggle() {
+    settingsMenu.classList.toggle("settings-menu-height")
+}
+
+// setting dark mode button //
+
+darkBtn.onclick = function () {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }
+}
+
+//local storage for darkmode//
+
+if (localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if (localStorage.getItem("theme") == "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else {
+    localStorage.setItem("theme", "light");
+}
+
+// footer //
+
+const d = new Date();
+document.getElementById("demo").innerHTML = d.getFullYear();
